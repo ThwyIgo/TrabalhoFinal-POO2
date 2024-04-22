822f5b89626d5bcaa75fd0c6f81cfbd83a5090c6
ThwyIgo <thiagopachecorocha@hotmail.com>
2024-04-22 12:31:46 -0300

    Mutual overlapping funcionando

--- Framework/src/main/java/Framework/visitors/CollisionVistor.java
+++ Framework/src/main/java/Framework/visitors/CollisionVistor.java
@@ -4,26 +4,16 @@ import Framework.sprite.Sprite;
 
 public class CollisionVistor {
     public static boolean mutualOverlapping(Sprite s1, Sprite s2){
-        int sx = s1.getX();
-        int sy = s1.getY();
-        int x = s2.getX();
-        int y = s2.getY();
+        int esqS1 = s1.getX(),
+                dirS1 = s1.getX() + s1.getImageWidth(),
+                cimaS1 = s1.getY(),
+                baixoS1 = s1.getY() + s1.getImageHeight();
 
-        boolean flag = x >= (sx) && x <= (sx + s2.getImageWidth()) && y >= (sy) && y <= (sy + s2.getImageHeight());
-        flag = flag || (x+s2.getImageWidth()) >= (sx) && (x+s2.getImageWidth()) <= (sx + s2.getImageWidth()) && y >= (sy) && y <= (sy + s2.getImageHeight());
-        flag = flag || x >= (sx) && x <= (sx + s2.getImageWidth()) && (y+s2.getImageHeight()) >= (sy) && (y+s2.getImageHeight()) <= (sy + s2.getImageHeight());
-        flag = flag || (x+s2.getImageWidth()) >= (sx) && (x+s2.getImageWidth()) <= (sx + s2.getImageWidth()) && (y+s2.getImageHeight()) >= (sy) && (y+s2.getImageHeight()) <= (sy + s2.getImageHeight());
+        int esqS2 = s2.getX(),
+                dirS2 = s2.getX() + s2.getImageWidth(),
+                cimaS2 = s2.getY(),
+                baixoS2 = s2.getY() + s2.getImageHeight();
 
-        sx = s2.getX();
-        sy = s2.getY();
-        x = s1.getX();
-        y = s1.getY();
-
-        boolean flag1 = x >= (sx) && x <= (sx + s1.getImageWidth()) && y >= (sy) && y <= (sy + s1.getImageHeight());
-        flag1 = flag1 || (x+s1.getImageWidth()) >= (sx) && (x+s1.getImageWidth()) <= (sx + s1.getImageWidth()) && y >= (sy) && y <= (sy + s1.getImageHeight());
-        flag1 = flag1 || x >= (sx) && x <= (sx + s1.getImageWidth()) && (y+s1.getImageHeight()) >= (sy) && (y+s1.getImageHeight()) <= (sy + s1.getImageHeight());
-        flag1 = flag1 || (x+s1.getImageWidth()) >= (sx) && (x+s1.getImageWidth()) <= (sx + s1.getImageWidth()) && (y+s1.getImageHeight()) >= (sy) && (y+s1.getImageHeight()) <= (sy + s1.getImageHeight());
-
-        return flag || flag1;
+        return esqS1 < dirS2 && dirS1 > esqS2 && cimaS1 < baixoS2 && baixoS1 > cimaS2;
     }
 }
